d7d1a65d171cdd06c2d5fb50fac8565ec0cc7aee
Andrei <evohuntmaster@gmail.com>
2019-09-22 19:48:15 +0300

    Register page added
    An initial register page has been created as a template

--- src/main/java/InTime/Main.java
+++ src/main/java/InTime/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        URL url = new File("src/main/java/InTime/view/login.fxml").toURI().toURL();
+        URL url = new File("src/main/java/InTime/view/register.fxml").toURI().toURL();
         Parent root = FXMLLoader.load(url);
         primaryStage.setTitle("InTime ALPHA");
         primaryStage.setScene(new Scene(root, 900, 500));
